7e3d1890fe68a2042407645469e23271e2f2d7ef
Guptavivek27 <vgupta16@depaul.edu>
2024-06-18 14:16:22 -0500

    Neurobazaar phase 1

---
+++ home/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from django.contrib import admin
+from home import views, ml
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('login', views.loginUser, name='login'),
+    path('logout', views.logoutUser, name='logout'),
+    path('register', views.register, name='register'),
+    path('upload/', views.upload, name='upload'),
+    path('results/', views.results, name='results'),
+    path('datasets/', views.datasets, name='datasets'),
+    path('download/<str:file>/<str:folder>/<str:visibility>/', views.download_file, name='download_file'),
+    path('update_reaction/', views.update_reaction, name='update_reaction'),
+    path('process_pca/', ml.process_pca, name='process_pca'),
+    path('serve_pca_plot/<str:username>/<int:workspace_number>/', views.serve_pca_plot, name='serve_pca_plot'),
+    # path('workspace/<str:username>/<int:workspace_number>/<str:filename>/', views.serve_workspace_image, name='serve_workspace_image'),
+
+
+]

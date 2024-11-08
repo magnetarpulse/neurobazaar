fb82c127e7546d87c57391488586ab5fdeafbb02
Areena Mahek <63315474+AreenaMahek@users.noreply.github.com>
2024-11-07 19:40:49 -0600

    Update views.py

--- home/views.py
+++ home/views.py
@@ -12,8 +12,7 @@ def get_config():
 
 
 
-
-def image_list(request):
+'''def image_list(request):
     media_folder=os.path.join(settings.MEDIA_ROOT,'lidc_pixConvImg')
     images = [f"{request.build_absolute_uri(settings.MEDIA_URL)}lidc_pixConvImg/{img}" for img in os.listdir(media_folder) if img.endswith(('.png', '.jpg', '.jpeg'))]  # Filter for image files
     #print(f"Images:{images}")  # Debug: Check the list of images
@@ -24,7 +23,7 @@ def dicom_list(request):
     dicom_folder=os.path.join(settings.MEDIA_ROOT,'dicom_images')
     dicom_images = [f"{request.build_absolute_uri(settings.MEDIA_URL)}dicom_images/{di_img}" for di_img in os.listdir(dicom_folder) if di_img.endswith(('.png', '.jpg', '.jpeg'))]  # Filter for dicom files
     #print(f"Images:{dicom_images}")  # Debug: Check the list of images
-    return JsonResponse({'dicom_images': dicom_images})
+    return JsonResponse({'dicom_images': dicom_images})'''
 
 
 

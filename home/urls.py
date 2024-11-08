5dfe88f7dc558fafa3a169b4b0333b5aa087f77a
Areena Mahek <63315474+AreenaMahek@users.noreply.github.com>
2024-11-07 19:40:09 -0600

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -7,7 +7,7 @@ from . import views
 
 
 urlpatterns = [
-    path('images/', views.image_list, name='image_list'),  # URL to get image filenames
-    path('dicom/', views.dicom_list, name='dicom_list'),  # URL to get DICOM filenames
+    #path('images/', views.image_list, name='image_list'),  # URL to get image filenames
+    #path('dicom/', views.dicom_list, name='dicom_list'),  # URL to get DICOM filenames
 ]
 
